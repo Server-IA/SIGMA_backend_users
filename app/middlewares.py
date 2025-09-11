c1f63843ad7078d4805d9017b5e3842670f467f9
Juan Andres Veru Sarmiento <85792843+JuanVeru@users.noreply.github.com>
2025-09-11 10:24:05 -0500

    Update middlewares.py

--- app/middlewares.py
+++ app/middlewares.py
@@ -32,7 +32,10 @@ def setup_middlewares(app):
     # Configuración CORS
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:3000","https://localhost:3000"],  # Cambiar a dominios específicos en producción
+        allow_origins=[
+        os.getenv("FRONTEND_PRUEBAS_URL"),
+        os.getenv("FRONTEND_URL"),
+        ],  # Cambiar a dominios específicos en producción
         allow_credentials=True,
         allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
         allow_headers=["Authorization", "Content-Type", "X-Request-ID"],
